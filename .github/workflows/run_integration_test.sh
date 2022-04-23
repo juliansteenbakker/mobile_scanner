92255487feb24c6a480f1d29289aa749bb370009
nilsreichardt <nils@reichardt.io>
2022-04-23 23:57:58 +0200

    Remove

--- .github/workflows/run_integration_test.sh
+++ .github/workflows/run_integration_test.sh
@@ -6,12 +6,6 @@ flutter devices
 # Navigate to example
 cd example
 
-echo "pwd"
-pwd
-
-echo "ls"
-ls
-
 # Insert QR Code image from "integration_test/qr_code.jpg" into the Android
 # camera emulator.
 echo "" >> ~/Library/Android/sdk/emulator/resources/Toren1BD.posters
@@ -20,6 +14,6 @@ echo "size 2 2" >> ~/Library/Android/sdk/emulator/resources/Toren1BD.posters
 echo "position 0 0 -1.8" >> ~/Library/Android/sdk/emulator/resources/Toren1BD.posters
 echo "rotation 0 0 0" >> ~/Library/Android/sdk/emulator/resources/Toren1BD.posters
 echo "default qr_code.jpg" >> ~/Library/Android/sdk/emulator/resources/Toren1BD.posters
-mv integration_test/qr_code.jpg ~/Library/Android/sdk/emulator/resources/qr_code.jpg
+mv integration_test/qr_code.jpg ~/Library/Android/sdk/emulator/resources/
 
 flutter run integration_test/app_test.dart -d emulator-5554
