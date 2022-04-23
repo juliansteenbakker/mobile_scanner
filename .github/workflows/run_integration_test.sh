aaeb0472215472e2f94c8e3e51893dc7e96a5181
nilsreichardt <nils@reichardt.io>
2022-04-23 23:56:36 +0200

    Add `/Library`

--- .github/workflows/run_integration_test.sh
+++ .github/workflows/run_integration_test.sh
@@ -14,12 +14,12 @@ ls
 
 # Insert QR Code image from "integration_test/qr_code.jpg" into the Android
 # camera emulator.
-echo "" >> ~/Android/sdk/emulator/resources/Toren1BD.posters
-echo "poster custom" >> ~/Android/sdk/emulator/resources/Toren1BD.posters
-echo "size 2 2" >> ~/Android/sdk/emulator/resources/Toren1BD.posters
-echo "position 0 0 -1.8" >> ~/Android/sdk/emulator/resources/Toren1BD.posters
-echo "rotation 0 0 0" >> ~/Android/sdk/emulator/resources/Toren1BD.posters
-echo "default qr_code.jpg" >> ~/Android/sdk/emulator/resources/Toren1BD.posters
-mv integration_test/qr_code.jpg ~/Android/sdk/emulator/resources/qr_code.jpg
+echo "" >> ~/Library/Android/sdk/emulator/resources/Toren1BD.posters
+echo "poster custom" >> ~/Library/Android/sdk/emulator/resources/Toren1BD.posters
+echo "size 2 2" >> ~/Library/Android/sdk/emulator/resources/Toren1BD.posters
+echo "position 0 0 -1.8" >> ~/Library/Android/sdk/emulator/resources/Toren1BD.posters
+echo "rotation 0 0 0" >> ~/Library/Android/sdk/emulator/resources/Toren1BD.posters
+echo "default qr_code.jpg" >> ~/Library/Android/sdk/emulator/resources/Toren1BD.posters
+mv integration_test/qr_code.jpg ~/Library/Android/sdk/emulator/resources/qr_code.jpg
 
 flutter run integration_test/app_test.dart -d emulator-5554
