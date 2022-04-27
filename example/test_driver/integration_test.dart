5aa359c6c15607a8f9ff7d65701a9b7fac228938
nilsreichardt <nils@reichardt.io>
2022-04-27 12:28:08 +0200

    Better docs for granting access

--- example/test_driver/integration_test.dart
+++ example/test_driver/integration_test.dart
@@ -3,8 +3,18 @@ import 'dart:io';
 import 'package:integration_test/integration_test_driver.dart';
 
 Future<void> main() async {
-  // Grand access to camera with adb, because the Flutter integration test
-  // can't interact with native buttons.
+  // Grant access to camera with adb, because the Flutter integration test can't
+  // interact with native buttons.
+  //
+  // It's only a workaround for https://github.com/flutter/flutter/issues/12561.
+  //
+  // We need to grant the access to the camera at this location. A better
+  // location would be in CI setup (like in the `run_integration.sh`). But this
+  // not possible because in the CI the app is not installed when calling the
+  // adb command. Therefore, granting the access will not work. A different good
+  // location would be the setUpAll method in the app_test.dart file. But this
+  // is also not possible, because this command will be executed from Android
+  // app inside the emulator, which will throw an `PermissionDenied` error.
   await Process.run('adb', [
     'shell',
     'pm',
