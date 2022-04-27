7ed1e2a0dfabd016142a99c5cc853f50fcb7b6d6
nilsreichardt <nils@reichardt.io>
2022-04-27 13:01:11 +0200

    Without emulator setup

--- .github/workflows/run_integration_test.sh
+++ .github/workflows/run_integration_test.sh
@@ -3,23 +3,6 @@
 # Navigate to example
 cd example
 
-# Insert QR Code image from "integration_test/qr_code.jpg" into the Android
-# camera emulator.
-echo "" >> ~/Library/Android/sdk/emulator/resources/Toren1BD.posters
-echo "poster qr_code" >> ~/Library/Android/sdk/emulator/resources/Toren1BD.posters
-echo "size 2 2" >> ~/Library/Android/sdk/emulator/resources/Toren1BD.posters
-echo "position 0 0 -1.8" >> ~/Library/Android/sdk/emulator/resources/Toren1BD.posters
-echo "rotation 0 0 0" >> ~/Library/Android/sdk/emulator/resources/Toren1BD.posters
-echo "default qr_code.jpg" >> ~/Library/Android/sdk/emulator/resources/Toren1BD.posters
-
-echo "cat ~/Library/Android/sdk/emulator/resources/Toren1BD.posters"
-
-mv integration_test/qr_code.jpg ~/Library/Android/sdk/emulator/resources/
-
-pushd ~/Library/Android/sdk/emulator/resources/
-ls
-popd
-
 # We can't use the "flutter test" command, because it will not execute the adb
 # command to access the camera (defined in the "test_driver/integration_test.jpg")
 flutter drive --driver=test_driver/integration_test.dart --target=integration_test/app_test.dart -d emulator-5554
