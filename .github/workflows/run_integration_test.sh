c4cddbfd3d30fc01db2cd74a496aed25e6df32c8
nilsreichardt <nils@reichardt.io>
2022-04-27 17:56:47 +0200

    Use action fork

--- .github/workflows/run_integration_test.sh
+++
@@ -1,8 +0,0 @@
-# #!/bin/bash
-
-# Navigate to example
-cd example
-
-# We can't use the "flutter test" command, because it will not execute the adb
-# command to access the camera (defined in the "test_driver/integration_test.jpg")
-flutter drive --driver=test_driver/integration_test.dart --target=integration_test/app_test.dart -d emulator-5554
