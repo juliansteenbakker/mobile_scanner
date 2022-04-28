61032e22bbe4ccf37c9434451cf08802117b32a3
nilsreichardt <nils@reichardt.io>
2022-04-28 16:32:58 +0200

    Add screenshot on failure

--- example/integration_test/app_test.dart
+++ example/integration_test/app_test.dart
@@ -4,7 +4,8 @@ import 'package:integration_test/integration_test.dart';
 import 'package:mobile_scanner_example/main.dart';
 
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized()
+      as IntegrationTestWidgetsFlutterBinding;
 
   // Before running the test, add the screenshot "qr_code.jpg" to your Android
   // emulator camera: https://stackoverflow.com/a/64922184/8358501
@@ -16,8 +17,20 @@ void main() {
       await tester.tap(find.text('MobileScanner with Controller'));
       await tester.pumpAndSettle();
 
-      const link = 'https://sharez.one/pGmfH4rTQeuxXbLE6';
-      await waitFor(tester, find.text(link));
+      const link = 'https://sharez.one/pGmfH4rTQeuxXbLE6_';
+      try {
+        await waitFor(
+          tester,
+          find.text(link),
+          timeout: const Duration(seconds: 20),
+        );
+      } catch (e) {
+        // This is required prior to taking the screenshot (Android only).
+        await binding.convertFlutterSurfaceToImage();
+        await tester.pump();
+        await binding.takeScreenshot('screenshot-1');
+      }
+
       expect(
         find.text(link),
         findsOneWidget,
@@ -28,10 +41,22 @@ void main() {
 
 /// Wait for the [finder] to appear.
 ///
+/// Throws an [Exception] when the timeout is reached.
+///
 /// Workaround for the old FlutterDriver.waitFor. Source:
 /// https://github.com/flutter/flutter/issues/88765
-Future<void> waitFor(WidgetTester tester, Finder finder) async {
+Future<void> waitFor(
+  WidgetTester tester,
+  Finder finder, {
+  Duration timeout = const Duration(seconds: 20),
+}) async {
+  final end = DateTime.now().add(timeout);
+
   do {
+    if (DateTime.now().isAfter(end)) {
+      throw Exception('Timed out waiting for $finder');
+    }
+
     await tester.pumpAndSettle();
     await Future.delayed(const Duration(milliseconds: 100));
   } while (finder.evaluate().isEmpty);
--- example/test_driver/integration_test.dart
+++ example/test_driver/integration_test.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:integration_test/integration_test_driver.dart';
+import 'package:integration_test/integration_test_driver_extended.dart';
 
 Future<void> main() async {
   // Grant access to camera with adb, because the Flutter integration test can't
@@ -23,5 +23,17 @@ Future<void> main() async {
     'android.permission.CAMERA'
   ]);
 
-  await integrationDriver();
+  await integrationDriver(
+    onScreenshot: (String screenshotName, List<int> screenshotBytes) async {
+      final image = await File('test_results/$screenshotName.png').create(
+        // Create the folder "test_results" if it doesn't exist.
+        recursive: true,
+      );
+
+      image.writeAsBytesSync(screenshotBytes);
+
+      // Return false if the screenshot is invalid.
+      return true;
+    },
+  );
 }
