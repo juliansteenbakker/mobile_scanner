cda5965a6849d837f32f292377a9de6fce8d2e29
nilsreichardt <nils@reichardt.io>
2022-05-04 00:07:25 +0200

    Increase number of screenshots

--- .github/workflows/make_screenshots.sh
+++ .github/workflows/make_screenshots.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # We assume that the build takes longer than 200 seconds.
-sleep 200
+sleep 300
 
-for i in {1..100}
+for i in {1..1000}
 do
    adb shell screencap -p > test_results/$i.jpg
    sleep 1
