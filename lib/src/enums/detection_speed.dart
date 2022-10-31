7e2a4b7ccd5f87959142075e4031540abc4e9da7
Julian Steenbakker <juliansteenbakker@outlook.com>
2022-10-31 20:44:03 +0100

    style: format

--- lib/src/enums/detection_speed.dart
+++ lib/src/enums/detection_speed.dart
@@ -1,6 +1,5 @@
 /// The detection speed of the scanner.
 enum DetectionSpeed {
-
   /// The scanner will only scan a barcode once, and never again until another
   /// barcode has been scanned.
   noDuplicates,
