38b3d5c353d8fc49ecb7b752b23e456b9aa1e016
Navaron Bracke <brackenavaron@gmail.com>
2022-11-19 18:28:14 +0100

    add error details to mobile scanner error

--- lib/src/mobile_scanner_exception.dart
+++ lib/src/mobile_scanner_exception.dart
@@ -2,8 +2,32 @@ import 'package:mobile_scanner/src/enums/mobile_scanner_error_code.dart';
 
 /// This class represents an exception throzn by the mobile scanner.
 class MobileScannerException implements Exception {
-  MobileScannerException({required this.errorCode});
+  const MobileScannerException({
+    required this.errorCode,
+    this.errorDetails,
+  });
 
   /// The error code of the exception.
   final MobileScannerErrorCode errorCode;
+
+  /// The additional error details that came with the [errorCode].
+  final MobileScannerErrorDetails? errorDetails;
+}
+
+/// The raw error details for a [MobileScannerException].
+class MobileScannerErrorDetails {
+  const MobileScannerErrorDetails({
+    this.code,
+    this.details,
+    this.message,
+  });
+
+  /// The error code from the [PlatformException].
+  final String? code;
+
+  /// The details from the [PlatformException].
+  final Object? details;
+
+  /// The error message from the [PlatformException].
+  final String? message;
 }
