4cba1c12c2206dbd633fe60d0fc2352ae9a85da9
p-mazhnik <pavel.mazhnik@gmail.com>
2022-11-22 15:54:29 +0300

    add toString method to MobileScannerException

--- lib/src/mobile_scanner_exception.dart
+++ lib/src/mobile_scanner_exception.dart
@@ -1,4 +1,9 @@
 class MobileScannerException implements Exception {
   String message;
   MobileScannerException(this.message);
+
+  @override
+  String toString() {
+    return "MobileScannerException: $message";
+  }
 }
