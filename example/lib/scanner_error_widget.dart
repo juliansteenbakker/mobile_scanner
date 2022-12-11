1629e1593d0e4ee8f864058d0cc5df634c4a1578
Navaron Bracke <brackenavaron@gmail.com>
2022-12-11 20:27:20 +0100

    add mobile scanner error widget to example

---
+++ example/lib/scanner_error_widget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+class ScannerErrorWidget extends StatelessWidget {
+  const ScannerErrorWidget({Key? key, required this.error}) : super(key: key);
+
+  final MobileScannerException error;
+
+  @override
+  Widget build(BuildContext context) {
+    String errorMessage;
+
+    switch (error.errorCode) {
+      case MobileScannerErrorCode.controllerUninitialized:
+        errorMessage = 'Controller not ready.';
+        break;
+      case MobileScannerErrorCode.permissionDenied:
+        errorMessage = 'Permission denied';
+        break;
+      default:
+        errorMessage = 'Generic Error';
+        break;
+    }
+
+    return ColoredBox(
+      color: Colors.black,
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(bottom: 16),
+              child: Icon(Icons.error, color: Colors.white),
+            ),
+            Text(
+              errorMessage,
+              style: const TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
