a2ac74505e312a50f83b3bd89ca39e3dddc528de
p-mazhnik <pavel.mazhnik@gmail.com>
2023-02-28 11:54:24 +0530

    fix(web): waiting for js libs to load

--- lib/mobile_scanner_web_plugin.dart
+++ lib/mobile_scanner_web_plugin.dart
@@ -26,7 +26,7 @@ class MobileScannerWebPlugin {
     );
     final MobileScannerWebPlugin instance = MobileScannerWebPlugin();
 
-    injectJSLibraries(barCodeReader.jsLibraries);
+    _jsLibrariesLoadingFuture = injectJSLibraries(barCodeReader.jsLibraries);
 
     channel.setMethodCallHandler(instance.handleMethodCall);
     event.setController(instance.controller);
@@ -55,8 +55,11 @@ class MobileScannerWebPlugin {
       ZXingBarcodeReader(videoContainer: vidDiv);
   StreamSubscription? _barCodeStreamSubscription;
 
+  static late Future _jsLibrariesLoadingFuture;
+
   /// Handle incomming messages
   Future<dynamic> handleMethodCall(MethodCall call) async {
+    await _jsLibrariesLoadingFuture;
     switch (call.method) {
       case 'start':
         return _start(call.arguments as Map);
