6ff77dbc3120b698cc7de0bd510a8ee10adc7360
Guillaume VARA <guillaume.vara@gmail.com>
2023-05-13 12:20:00 +0200

    Update zxing.dart

--- lib/src/web/zxing.dart
+++ lib/src/web/zxing.dart
@@ -170,7 +170,7 @@ extension JsZXingBrowserMultiFormatReaderExt
 
 const zxingJsLibrary = JsLibrary(
   contextName: 'ZXing',
-  url: 'https://unpkg.com/@zxing/library@0.19.1',
+  url: 'https://unpkg.com/@zxing/library@0.20.0',
   usesRequireJs: true,
 );
 
