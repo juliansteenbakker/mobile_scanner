2dfd5cf1d566af695845d470426a856e9c82d913
Navaron Bracke <brackenavaron@gmail.com>
2023-10-22 19:48:57 +0200

    convert email type into enhanced enum

--- lib/src/enums/email_type.dart
+++ lib/src/enums/email_type.dart
@@ -1,17 +1,28 @@
 /// Email format type constants.
 enum EmailType {
   /// Unknown email type.
-  ///
-  /// Constant Value: 0
-  unknown,
+  unknown(0),
 
   /// Work email.
-  ///
-  /// Constant Value: 1
-  work,
+  work(1),
 
   /// Home email.
-  ///
-  /// Constant Value: 2
-  home,
+  home(2);
+
+  const EmailType(this.rawValue);
+
+  factory EmailType.fromRawValue(int value) {
+    switch (value) {
+      case 1:
+        return EmailType.work;
+      case 2:
+        return EmailType.home;
+      case 0:
+      default:
+        return EmailType.unknown;
+    }
+  }
+
+  /// The raw email type value.
+  final int rawValue;
 }
