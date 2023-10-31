b1ac9c710b792919b505379a5c35618b861cea4f
Navaron Bracke <brackenavaron@gmail.com>
2023-10-31 11:09:01 +0100

    remove Swift torch errors from the error enum

--- ios/Classes/MobileScannerError.swift
+++ ios/Classes/MobileScannerError.swift
@@ -10,9 +10,7 @@ enum MobileScannerError: Error {
     case noCamera
     case alreadyStarted
     case alreadyStopped
-    case torchError(_ error: Error)
     case cameraError(_ error: Error)
-    case torchWhenStopped
     case zoomWhenStopped
     case zoomError(_ error: Error)
     case analyzerError(_ error: Error)
