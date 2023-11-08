53ba133cb2a239c34606f49dff3e906e31751826
Navaron Bracke <brackenavaron@gmail.com>
2023-11-08 14:41:32 +0100

    revert the addition of platform specific view attributes

--- lib/src/mobile_scanner_view_attributes.dart
+++ lib/src/mobile_scanner_view_attributes.dart
@@ -1,10 +1,15 @@
 import 'dart:ui';
 
-/// This interface defines the attributes for the mobile scanner view.
-abstract class MobileScannerViewAttributes {
+/// This class defines the attributes for the mobile scanner view.
+class MobileScannerViewAttributes {
+  const MobileScannerViewAttributes({
+    required this.hasTorch,
+    required this.size,
+  });
+
   /// Whether the current active camera has a torch.
-  bool get hasTorch;
+  final bool hasTorch;
 
   /// The size of the camera output.
-  Size get size;
+  final Size size;
 }
