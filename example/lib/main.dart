9c01955c569534912cfd99a30eb3007d94881c56
Navaron Bracke <brackenavaron@gmail.com>
2023-11-16 16:08:34 +0100

    remove obsolete sized box

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -23,85 +23,80 @@ class MyHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Mobile Scanner Example')),
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const BarcodeScannerListView(),
-                  ),
-                );
-              },
-              child: const Text('MobileScanner with ListView'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const BarcodeScannerWithController(),
-                  ),
-                );
-              },
-              child: const Text('MobileScanner with Controller'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const BarcodeScannerWithScanWindow(),
-                  ),
-                );
-              },
-              child: const Text('MobileScanner with ScanWindow'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const BarcodeScannerReturningImage(),
-                  ),
-                );
-              },
-              child:
-                  const Text('MobileScanner with Controller (returning image)'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const BarcodeScannerWithZoom(),
-                  ),
-                );
-              },
-              child: const Text('MobileScanner with zoom slider'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const BarcodeScannerPageView(),
-                  ),
-                );
-              },
-              child: const Text('MobileScanner pageView'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => BarcodeScannerWithOverlay(),
-                  ),
-                );
-              },
-              child: const Text('MobileScanner with Overlay'),
-            ),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const BarcodeScannerListView(),
+                ),
+              );
+            },
+            child: const Text('MobileScanner with ListView'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const BarcodeScannerWithController(),
+                ),
+              );
+            },
+            child: const Text('MobileScanner with Controller'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const BarcodeScannerWithScanWindow(),
+                ),
+              );
+            },
+            child: const Text('MobileScanner with ScanWindow'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const BarcodeScannerReturningImage(),
+                ),
+              );
+            },
+            child: const Text('MobileScanner with Controller (returning image)'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const BarcodeScannerWithZoom(),
+                ),
+              );
+            },
+            child: const Text('MobileScanner with zoom slider'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const BarcodeScannerPageView(),
+                ),
+              );
+            },
+            child: const Text('MobileScanner pageView'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => BarcodeScannerWithOverlay(),
+                ),
+              );
+            },
+            child: const Text('MobileScanner with Overlay'),
+          ),
+        ],
       ),
     );
   }
