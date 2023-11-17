87bd6bd210e5212af7e41c5daa3b89621300f08f
Navaron Bracke <brackenavaron@gmail.com>
2023-11-17 13:26:22 +0100

    update the result point js interop definition

---
+++ lib/src/web/zxing/result_point.dart
@@ -0,0 +1,28 @@
+import 'dart:js_interop';
+
+/// The JS static interop class for the Result class in the ZXing library.
+///
+/// See also: https://github.com/zxing-js/library/blob/master/src/core/ResultPoint.ts
+@JS()
+@staticInterop
+abstract class Result {}
+
+extension ResultPointExt on Result {
+  external JSFunction getX;
+
+  external JSFunction getY;
+
+  /// The x coordinate of the point.
+  double get x {
+    final JSNumber? x = getX.callAsFunction() as JSNumber?;
+
+    return x?.toDartDouble ?? 0;
+  }
+
+  /// The y coordinate of the point.
+  double get y {
+    final JSNumber? y = getY.callAsFunction() as JSNumber?;
+
+    return y?.toDartDouble ?? 0;
+  }
+}
