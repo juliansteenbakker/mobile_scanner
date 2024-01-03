a08dc6d8958e86d6f7c261fa715f6b3a84eda8c2
Navaron Bracke <brackenavaron@gmail.com>
2024-01-03 17:13:27 +0100

    fix small align issue in example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -23,82 +23,84 @@ class MyHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Mobile Scanner Example')),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => const BarcodeScannerListView(),
-                ),
-              );
-            },
-            child: const Text('MobileScanner with ListView'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => const BarcodeScannerWithController(),
-                ),
-              );
-            },
-            child: const Text('MobileScanner with Controller'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => const BarcodeScannerWithScanWindow(),
-                ),
-              );
-            },
-            child: const Text('MobileScanner with ScanWindow'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => const BarcodeScannerReturningImage(),
-                ),
-              );
-            },
-            child: const Text(
-              'MobileScanner with Controller (returning image)',
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const BarcodeScannerListView(),
+                  ),
+                );
+              },
+              child: const Text('MobileScanner with ListView'),
             ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => const BarcodeScannerWithZoom(),
-                ),
-              );
-            },
-            child: const Text('MobileScanner with zoom slider'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => const BarcodeScannerPageView(),
-                ),
-              );
-            },
-            child: const Text('MobileScanner pageView'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => BarcodeScannerWithOverlay(),
-                ),
-              );
-            },
-            child: const Text('MobileScanner with Overlay'),
-          ),
-        ],
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const BarcodeScannerWithController(),
+                  ),
+                );
+              },
+              child: const Text('MobileScanner with Controller'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const BarcodeScannerWithScanWindow(),
+                  ),
+                );
+              },
+              child: const Text('MobileScanner with ScanWindow'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const BarcodeScannerReturningImage(),
+                  ),
+                );
+              },
+              child: const Text(
+                'MobileScanner with Controller (returning image)',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const BarcodeScannerWithZoom(),
+                  ),
+                );
+              },
+              child: const Text('MobileScanner with zoom slider'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const BarcodeScannerPageView(),
+                  ),
+                );
+              },
+              child: const Text('MobileScanner pageView'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => BarcodeScannerWithOverlay(),
+                  ),
+                );
+              },
+              child: const Text('MobileScanner with Overlay'),
+            ),
+          ],
+        ),
       ),
     );
   }
