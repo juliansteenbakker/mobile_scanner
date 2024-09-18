b73aa67dcbfcaf267f8b1c4f663a8ae0715c0440
Navaron Bracke <brackenavaron@gmail.com>
2024-09-18 11:49:25 +0200

    add comment for enum class

--- ios/Classes/MobileScannerError.swift
+++ ios/Classes/MobileScannerError.swift
@@ -6,6 +6,12 @@
 //
 import Foundation
 
+// TODO: decide if we should keep or discard this enum
+// When merging the iOS / MacOS implementations we should either keep the enum or remove it
+
+// This enum is a bit of a leftover from older parts of the iOS implementation.
+// It is used by the handler that throws these error codes,
+// while the plugin class intercepts these and converts them to `FlutterError()`s.
 enum MobileScannerError: Error {
     case noCamera
     case alreadyStarted
