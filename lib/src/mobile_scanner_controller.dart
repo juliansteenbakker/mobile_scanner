25e52d788c47aa326b40a1d2e2257b8b23d76b9e
Rafa Ruiz <raruizmu@gmail.com>
2024-10-14 20:24:30 +0200

    Update lib/src/mobile_scanner_controller.dart
    Co-authored-by: Navaron Bracke <brackenavaron@gmail.com>

--- lib/src/mobile_scanner_controller.dart
+++ lib/src/mobile_scanner_controller.dart
@@ -83,7 +83,13 @@ class MobileScannerController extends ValueNotifier<MobileScannerState> {
   /// Defaults to false, and is only supported on iOS, MacOS and Android.
   final bool returnImage;
 
-  /// Whether the image should be inverted in intervals (original - inverted - original…)
+  /// Whether the scanner should try to detect color-inverted barcodes in every other frame.
+  ///
+  /// When this option is enabled, every odd frame from the camera preview has its colors inverted before processing.
+  /// This is useful if barcodes can be both black-on-white (the most common) and white-on-black (less common).
+  /// Usage of this parameter can incur a performance cost, as some frames need to be altered further during processing.
+  ///
+  /// Defaults to false and is only supported on Android and iOS.
   ///
   /// Defaults to false.
   final bool intervalInvertImage;
