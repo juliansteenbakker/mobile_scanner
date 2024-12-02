49bb42495164ac0ad6553b0ea2c763201f0b8f3e
Sander Roest <s.roest@dalosy.com>
2024-12-02 11:48:46 +0100

    Remove unnecessary changes

--- example/lib/barcode_scanner_window.dart
+++ example/lib/barcode_scanner_window.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:mobile_scanner_example/scanned_barcode_label.dart';
+
 import 'package:mobile_scanner_example/scanner_error_widget.dart';
 
 class BarcodeScannerWithScanWindow extends StatefulWidget {
