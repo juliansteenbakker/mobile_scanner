1df3470551755af7c471ddeaf4896d67b6d8f7bb
Julian Steenbakker <juliansteenbakker@outlook.com>
2025-02-27 21:27:18 +0100

    refactor: disable publishing of analyzeImage on barcode stream for now

--- android/src/main/kotlin/dev/steenbakker/mobile_scanner/MobileScannerHandler.kt
+++ android/src/main/kotlin/dev/steenbakker/mobile_scanner/MobileScannerHandler.kt
@@ -42,11 +42,12 @@ class MobileScannerHandler(
 
     private val analyzeImageSuccessCallback: AnalyzerSuccessCallback = {
         Handler(Looper.getMainLooper()).post {
-            // Also publish on controller result
-            barcodeHandler.publishEvent(mapOf(
-                "name" to "barcode",
-                "data" to it,
-            ))
+            // TODO: Open for discussion if we want to publish the results on the barcode stream as well.
+//            // Also publish on controller result
+//            barcodeHandler.publishEvent(mapOf(
+//                "name" to "barcode",
+//                "data" to it,
+//            ))
 
             analyzerResult?.success(mapOf(
                 "name" to "barcode",
