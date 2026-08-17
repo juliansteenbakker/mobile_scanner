ad34f7d021548d2c9b1b0bce6c5b42fbf64b4847
Julian Steenbakker <jst@beverinnovations.com>
2026-08-17 14:05:11 +0200

    chore: upgrade gradle to v9, groovy to kts

---
+++ example/android/settings.gradle.kts
@@ -0,0 +1,35 @@
+pluginManagement {
+    val flutterSdkPath =
+        run {
+            val properties = java.util.Properties()
+            file("local.properties").inputStream().use { properties.load(it) }
+            val flutterSdkPath = properties.getProperty("flutter.sdk")
+            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+            flutterSdkPath
+        }
+
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "9.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.4.10" apply false
+}
+
+// Replaces the `allprojects { repositories { } }` block that used to live in
+// build.gradle, which AGP 9 no longer allows.
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+include(":app")
